484fe97d526c26112d74a5e7edaad9184a6ebe0f
Layedra75 <layedra75@gmail.com>
2024-01-25 13:01:22 -0500

    Se agrego el calendario y el modal al hacer click en una fecha

---
+++ src/components/Modal/ModalCitas.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+const ModalCitas = ({
+  show,
+  onHide,
+  onSave,
+  selectedDate,
+  selectedHour,
+  setSelectedHour,
+}) => {
+  const [title] = useState("Cita Médica");
+  const [start, setStart] = useState(
+    selectedDate?.toISOString().slice(0, 10) || ""
+  );
+  const [hour, setHour] = useState(
+    selectedHour !== "" ? selectedHour.toString().padStart(2, "0") + ":00" : ""
+  );
+  const [patient, setPatient] = useState("");
+  const [doctor, setDoctor] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    setHour(selectedHour !== "" ? selectedHour.toString().padStart(2, "0") + ":00" : "");
+  }, [selectedHour]);
+
+  const handleSave = () => {
+    const fullStart = `${start}T${hour}`;
+    onSave(title, description, fullStart, null, patient, doctor);
+  };
+
+  return (
+    <Modal show={show} onHide={onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="form-group">
+          <label>Fecha</label>
+          <input
+            type="date"
+            className="form-control"
+            value={start}
+            onChange={(e) => setStart(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Hora</label>
+          <input
+            type="time"
+            className="form-control"
+            value={hour}
+            onChange={(e) => setHour(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Paciente</label>
+          <input
+            type="text"
+            className="form-control"
+            value={patient}
+            onChange={(e) => setPatient(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Doctor</label>
+          <input
+            type="text"
+            className="form-control"
+            value={doctor}
+            onChange={(e) => setDoctor(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Descripción (Opcional)</label>
+          <textarea
+            className="form-control"
+            rows="3"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onHide}>
+          Cancelar
+        </Button>
+        <Button variant="primary" onClick={handleSave}>
+          Guardar
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalCitas;
--- src/views/Citas/Citas.js
+++ src/views/Citas/Citas.js
@@ -1,8 +1,86 @@
-// Citas.js
-import React from "react";
+import React, { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import ModalCitas from "../../components/Modal/ModalCitas";
+import esLocale from "@fullcalendar/core/locales/es";
+import "./Citas.css";
 
-const Citas = () => {
-  return <div>Citas</div>;
+const Calendar = () => {
+  const [modalShow, setModalShow] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedHour, setSelectedHour] = useState("");
+
+  const handleDateClick = (arg) => {
+    const selectedDate = new Date(arg.date);
+    console.log("selectedDate:", selectedDate);
+    const selectedHour = selectedDate.getHours();
+    const selectedMinute = selectedDate.getMinutes();
+    const formattedHour = selectedHour.toString().padStart(2, "0");
+    const formattedMinute = selectedMinute.toString().padStart(2, "0");
+    const formattedTime = `${formattedHour}:${formattedMinute}`;
+    setSelectedDate(selectedDate);
+    setSelectedHour(formattedTime);
+    setModalShow(true);
+  };
+  
+  const handleSave = (title, description, start, end, patient, doctor) => {
+    setModalShow(false);
+    setEvents([
+      ...events,
+      {
+        title,
+        description,
+        start,
+        end,
+        patient,
+        doctor,
+      },
+    ]);
+  };
+
+  return (
+    <div className="card shadow border-0 calendar-card">
+      <div className="card-header border-0">
+        <h1 className="bg-light">Citas Médicas</h1>
+      </div>
+      <div className="card-body">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="timeGridWeek"
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          dateClick={handleDateClick}
+          events={events}
+          locale={esLocale}
+          slotMinTime="08:00:00"
+          slotMaxTime="18:00:00"
+          hiddenDays={[0]}
+          slotEventOverlap={false}
+          views={{
+            timeGrid: {
+              dayHeaderFormat: { weekday: "long" },
+              allDaySlot: false,
+            },
+          }}
+          timeZone="local"
+        />
+      </div>
+      <ModalCitas
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        onSave={handleSave}
+        selectedDate={selectedDate}
+        selectedHour={selectedHour}
+        setSelectedHour={setSelectedHour}
+      />
+    </div>
+  );
 };
 
-export default Citas;
+export default Calendar;
